084f32074d9af31860c14008db9efa0b1959ff57
Tyler-Arch-Laptop <tylerpopson@gmail.com>
2022-09-29 17:03:58 -0400

    started adding svg for banner animation

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,24 +1,18 @@
 <script setup lang="ts">
+    let screenW : Number = 0;
+    let screenH : Number = 0;
 
+    let startpath : String = "M0 0 L200 0 L250 50 L200 100 L0 100 L0 0 Z"
 </script>
 
 <template>
-    <div class="flex w-screen h-screen bg-moog-black -z-10 overflow-hidden">
-        <div class="flex flex-auto w-6/12 -skew-x-[30deg]">
-            <div class="skew-x-[30deg] flex flex-col w-full h-full items-center justify-center">
-                <div class="bg-moog-lavender w-1/2 h-1/2 rounded-full">
-                    img
-                </div>
-                <div class="text-moog-lavender text-5xl">
-                    Jordan Burger
-                </div>
-                <div class="text-moog-lavender text-3xl">
-                    Audio Engineer
-                </div>
-            </div>
+    <div class="flex w-screen h-screen -z-10 overflow-hidden">
+        <div class="flex flex-auto  w-1/2">
+            <svg height="100%" width="100%">
+                <path d=""/>
+            </svg>
         </div>
-        <div class="flex-auto w-1/12 bg-moog-blue -skew-x-[30deg]"></div>
-        <div class="flex-auto w-1/12 bg-moog-lavender -skew-x-[30deg]"></div>
-        <div class="flex-auto w-6/12 bg-moog-tan -skew-x-[30deg]"></div>
+        <div class="flex flex-auto w-1/2">
+        </div>
     </div>
 </template>
