a2acb96e41856d4eb2dccb010927d76756b93da5
Tyler-Arch-Laptop <tylerpopson@gmail.com>
2022-10-09 14:37:51 -0400

    removed spotify link in listen section

--- src/components/Listen.vue
+++ src/components/Listen.vue
@@ -5,20 +5,26 @@
 <template>
     <div id="listen" class="flex flex-col-reverse md:flex-row w-screen h-auto md:h-screen bg-moog-black">
         <div class="flex flex-col w-screen md:w-2/3 px-5 md:px-32 my-10 md:my-0 h-full bg-moog-black items-center justify-center">
+
             <iframe class="" style="border-radius:12px" src="https://open.spotify.com/embed/album/5FCefVJ1ACpDxruisklkE3?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
             <iframe class="" style="border-radius:12px" src="https://open.spotify.com/embed/album/5PJspByunI4lcrEwcOcvv4?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            
             <div class="flex flex-col md:flex-row w-full items-center justify-center">
-                <a href='https://soundcloud.com/jordan-burger-474132049' target="_blank" class="text-white hover:text-moog-lavender w-full md:w-1/2 text-center text-9xl transition-colors duration-300">
+
+                <div class="text-white font-overpassBold w-full md:w-1/2 text-center text-5xl">
+                    <h1>Listen On Soundcloud:</h1>
+                </div>
+
+                <a href='https://soundcloud.com/jordan-burger-474132049' target="_blank" class="text-white hover:text-moog-blue w-full md:w-1/2 text-center text-9xl transition-colors duration-300">
                     <font-awesome-icon icon="fa-brands fa-soundcloud" class=""/>
                 </a>
-                <a href='' target="_blank" class="text-white hover:text-moog-blue w-full md:w-1/2 text-center text-9xl transition-colors duration-300">
-
-                    <font-awesome-icon icon="fa-brands fa-spotify" class=""/>
-                </a>
             </div>
+
         </div>
+
         <div class="flex w-screen md:w-1/3 h-32 md:h-full bg-moog-lavender items-center justify-center">
             <h1 class="text-4xl font-overpassBold">Listen</h1>
         </div>
+
     </div>
 </template>
