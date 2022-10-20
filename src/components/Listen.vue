73c4ad57b358b05b6f9a8fd4dea358d0aef573fd
Tyler Popson <tylerpopson@gmail.com>
2022-10-20 11:50:13 +0000

    worked on visualizer animation

--- src/components/Listen.vue
+++ src/components/Listen.vue
@@ -1,49 +1,86 @@
 <script setup lang="ts">
     import { ref, onMounted } from 'vue';
     import type { Ref } from 'vue';
-    type point = {x: number, y: number};
+    type point = {x: Ref<number>, y: Ref<number>};
 
     const variance:number = 100;
     const visualizer: Ref<bar[]> = ref([]);
+    let frame:HTMLElement | null = null;
+    let fps: number = 10;
+    let now: any;
+    let then: any = Date.now();
+    let interval: any = 1000/fps;
+    let delta: any;
+    let animateElem = document.querySelector('animate');
+    
 
     class bar{
+        id: number;
         points: {p1:point, p2:point};
         color: string;
         stepAmount: Ref<number>;
+        target:Ref<number> = ref(Math.random()*200);
 
-        constructor(height:number, color:string){
+        constructor(id:number, height:number, color:string){
             this.points = {
-                p1: {x: 0, y: height},
-                p2: {x: Math.random()* 200, y: height}
+                p1: {x: ref(0), y: ref(height)},
+                p2: {x: ref(Math.random()*300-150), y: ref(height)}
             }
 
             this.color = color;
 
             this.stepAmount = ref(0);
+
+            this.id = id;
         }
 
-        animate = () => {
+        randomize = () => {
             
-            let val:number = 0;
+            this.target.value = Math.random()*200;
+        }
+    }
 
-            if(this.points.p2.y >= 300){
-                this.stepAmount.value = -1 * Math.abs(Math.random() * variance - (variance/2));
-            }
-            else if(this.points.p2.y <= 0){
-                this.stepAmount.value = Math.abs(Math.random() * variance - (variance/2));
-            }
-            else{
-                this.stepAmount.value = Math.random() * variance - (variance/2);
+    function animateVisualizer(){
+        
+        requestAnimationFrame(animateVisualizer);
+
+        now = Date.now();
+        delta = now - then;
+
+        if (delta > interval) {
+            for (let i: number = 0; i<visualizer.value.length; i++){
+                // visualizer.value[i].points.p1.y += 1;
+                // visualizer.value[i].points.p2.y += 1;
+
+                // visualizer.value[i].points.p2.x = Math.random()*30-15 + Math.sin(i* (Math.PI/25))*100+100 ;
+
+                // if(visualizer.value[i].points.p1.y > frame.clientHeight){
+                //     visualizer.value[i].points.p1.y = 0;
+                //     visualizer.value[i].points.p2.y = 0;
+                // }
             }
         }
     }
 
+    function rand(){
+        console.log("here");
+        for (let i: number = 0; i<visualizer.value.length; i++){
+                 visualizer.value[i].target.value = Math.random()*200;
+                
+            }
+    }
+
     onMounted(() => {
-        for (let i = 0; i < 100; i++) {
-            visualizer.value.push(new bar(i, "#000000"));
-            visualizer.value[i].animate();
+
+        frame = document.getElementById('frame');
+        if(frame != null){
+            for (let i = 0; i < 100; i++) {
+                visualizer.value.push(new bar(i, i * (frame.clientHeight / 100), "#000000"));
+            }
+            //animateVisualizer();
+            animateElem = document.querySelector('animate');
+            animateElem.addEventListener('endEvent', rand)
         }
-        
     });
 
 </script>
@@ -68,17 +105,27 @@
 
         </div>
 
-        <div class="flex relative w-screen md:w-1/3 h-32 md:h-full bg-moog-lavender items-center justify-center">
-            <svg class="flex absolute left-0 top-0 h-full w-2/5 bg-blue" height="100%" width="33%" v-for="item in visualizer">
-                <line :x1="item.points.p1.x" :y1="item.points.p1.y" :x2="item.points.p2.x" :y2="item.points.p2.y" style="stroke:rgb(0,0,0);stroke-width:2">
-                    <animate
-                        attributeName="x2"
-                        :values="item.point.p2.y;item.stepAmount;items.points.p2.y"
-                        repeatCount="indefinite"
-                        dur="1s"
-                        :onrepeat="item.animate"
-                    />
-                </line>
+        <div id="frame" class="flex relative w-screen md:w-1/3 h-32 md:h-full bg-moog-lavender items-center justify-center">
+            <svg  class="flex absolute left-0 top-0 h-full w-2/5 bg-blue" height="100%" width="33%">
+                <defs>
+                    <linearGradient id='gradient1' x1="0%" y1="0%" x2="100%" y2="0%" gradientUnits="userSpaceOnUse">
+                        <stop offset="0%" style="stop-color:#010d00;" />
+                        <!-- <stop offset="75%" style="stop-color:#B0D1D9;" /> -->
+                        <stop offset="75%" style="stop-color:#BF9663;" />
+                    </linearGradient>
+                </defs>
+                <g v-for="item in visualizer">
+                    <line id='bar' :x1="item.points.p1.x" :y1="item.points.p1.y" :x2="item.points.p2.x" :y2="item.points.p2.y" stroke="url(#gradient1)" style="stroke:url(#gradient1);stroke-width:6">
+                        <animate
+                            
+                            attributeName="x2"
+                            fill="freeze"
+                            :to="item.target"
+                            dur="1s" 
+                            repeatCount="infinite"
+                        />
+                    </line>
+                </g>
             </svg>
             <h1 class="text-4xl font-overpassBold">Listen</h1>
         </div>
