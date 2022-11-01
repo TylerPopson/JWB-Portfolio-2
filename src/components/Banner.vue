552c6f8f640c46740fee45f4571dc808c8a0935a
Tyler Popson <tylerpopson@gmail.com>
2022-11-01 18:01:45 +0000

    adjusted banner animation delays

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -70,19 +70,6 @@
             this.delta = this.now - this.then;
             
             if (this.delta > this.interval) {
-                // update time stuffs
-                
-                // Just `then = now` is not enough.
-                // Lets say we set fps at 10 which means
-                // each frame must take 100ms
-                // Now frame executes in 16ms (60fps) so
-                // the loop iterates 7 times (16*7 = 112ms) until
-                // delta > interval === true
-                // Eventually this lowers down the FPS as
-                // 112*10 = 1120ms (NOT 1000ms).
-                // So we have to get rid of that extra 12ms
-                // by subtracting delta (112) % interval (100).
-                // Hope that makes sense.
                 
                 this.then = this.now - (this.delta % this.interval);
                 
@@ -121,9 +108,9 @@
 
     async function animatePoints(){
         purplePentagon.animate();
-        await sleep(300);
+        await sleep(600);
         tealPentagon.animate();
-        await sleep(300);
+        await sleep(650);
         mainPentagon.animate();
     };
 
